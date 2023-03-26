5a629e24f8acb6c8424deec7d4eab9858b5a31ca
Minki Kim <alsrl0023@naver.com>
2023-03-26 22:05:07 +0900

    fix: 주석 제거 및 불필요한 코드 제거

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 import * as fs from "fs";
@@ -27,8 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
       });
 
       if (componentName) {
-        const folderPath =
-          vscode.workspace.workspaceFolders?.[0].uri.fsPath || "";
         const componentPath = path.join(fp, componentName);
 
         if (!fs.existsSync(componentPath)) {
@@ -60,5 +56,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
